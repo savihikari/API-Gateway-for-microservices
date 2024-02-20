c1b6082d7e82ab56a97e4aeb89a8b412bb2a8c20
savitap <savitap@W10PNTHNXTDG48.mavericsystems.lan>
2024-02-20 12:47:08 +0530

    initial commit

---
+++ src/main/java/com/training/org/FallBackMethodController.java
@@ -0,0 +1,18 @@
+package com.training.org;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+	@GetMapping("/userServiceFallBack")
+	public String userServiceFallBackMethod() {
+		return "User service is taking longer than Expected."+" Please try again later";
+	}
+	
+	@GetMapping("/departmentServiceFallBack")
+	public String departmentServiceFallBackMethod() {
+		return "Department service is taking longer than Expected."+" Please try again later";
+	}
+}
